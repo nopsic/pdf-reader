0d9df2013b15593eb58440917f331234b733dd71
Havasdi Dániel <nopsic@inf.elte.hu>
2021-10-05 19:51:46 +0200

    Create android pdf reader

---
+++ MyPdfReader/app/src/main/java/com/example/mypdfreader/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.mypdfreader
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var showBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        showBtn = findViewById(R.id.showBtn)
+
+        showBtn.setOnClickListener {
+            val intent = Intent(this, ViewPdfActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
